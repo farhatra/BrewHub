3603f811967d2a1cac948d8e13858ca45748fe6f
rabee farhat <rabeefarhat@live.com>
2023-12-06 00:24:51 +0100

    Commit: Add the API Project

---
+++ BrewHub.Persistence/PersistenceServiceRegistration.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BrewHub.Application.Contracts.Persistence;
+using BrewHub.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BrewHub.Persistence
+{
+    public static class PersistenceServiceRegistration
+    {
+        public static IServiceCollection AddPersistenceServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<BeerHubDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("BeerHubDbContextConnectionString")));
+
+            //services.AddScoped(typeof(IAsyncRepository<>), typeof(BaseRepository<>));
+
+            //one instance is created per request
+            services.AddScoped<IBreweryService, BreweryService>();
+            services.AddScoped<IWholesalerService, WholesalerService>();
+            services.AddScoped<IQuoteService, QuoteService>();
+
+            return services;
+        }
+    }
+}
