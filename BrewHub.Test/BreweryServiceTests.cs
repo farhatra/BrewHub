6bbab65a1107c6b86e5382a7cd28741998399ee8
rabee farhat <rabeefarhat@live.com>
2023-12-13 12:38:36 +0100

    commit: fix the xunit test

---
+++ BrewHub.Test/BreweryServiceTests.cs
@@ -0,0 +1,54 @@
+﻿using AutoMapper;
+using BrewHub.Application.Features.DtoModels;
+using BrewHub.Application.Profiles;
+using BrewHub.Persistence;
+using BrewHub.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace BrewHub.Test
+{
+    public class BreweryServiceTests
+    {
+        [Fact]
+        public async Task AddNewBeerAsync_ValidData_ShouldAddNewBeer()
+        {
+            // Arrange
+            var dbContext = GetInMemoryDbContext();
+            var mapper = GetMapper();
+            var breweryService = new BreweryService(dbContext, mapper);
+
+            var breweryId = 1;
+            var newBeerDto = new BeerDto { Name = "TestBeer", Type = "TestType", Price = 3.50m };
+
+            // Act
+            var result = await breweryService.AddNewBeerAsync(breweryId, newBeerDto);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(newBeerDto.Name, result.Name);
+            Assert.Equal(newBeerDto.Type, result.Type);
+            Assert.Equal(newBeerDto.Price, result.Price);
+        }
+
+        private static BeerHubDbContext GetInMemoryDbContext()
+        {
+            var options = new DbContextOptionsBuilder<BeerHubDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+
+            return new BeerHubDbContext(options);
+        }
+
+        private static IMapper GetMapper()
+        {
+            var config = new MapperConfiguration(cfg => cfg.AddProfile<MappingProfile>());
+            return config.CreateMapper();
+        }
+    }
+}
---
+++ BrewHub.Test/WholesalerServiceTests.cs
@@ -0,0 +1,56 @@
+﻿using AutoMapper;
+using BrewHub.Application.Profiles;
+using BrewHub.Persistence;
+using BrewHub.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace BrewHub.Test
+{
+    public class WholesalerServiceTests
+    {
+        [Fact]
+        public async Task AddSaleToWholesalerAsync_ValidData_ShouldAddSaleToWholesaler()
+        {
+            // Arrange
+            var dbContext = GetInMemoryDbContext();
+            var mapper = GetMapper();
+            var wholesalerService = new WholesalerService(dbContext, mapper);
+
+            var wholesalerId = 1;
+            var beerId = 1;
+            var quantity = 5;
+
+            // Act
+            await wholesalerService.AddSaleToWholesalerAsync(wholesalerId, beerId, quantity);
+
+            // Assert
+            var wholesaler = await dbContext.Wholesalers.FindAsync(wholesalerId);
+            var wholesalerBeer = wholesaler.WholesalerBeers.FirstOrDefault(wb => wb.BeerId == beerId);
+
+            Assert.NotNull(wholesalerBeer);
+            Assert.Equal(10 - quantity, wholesalerBeer.RemainingQuantity);
+        }
+
+        private static BeerHubDbContext GetInMemoryDbContext()
+        {
+            var options = new DbContextOptionsBuilder<BeerHubDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+
+            return new BeerHubDbContext(options);
+        }
+
+        private static IMapper GetMapper()
+        {
+            var config = new MapperConfiguration(cfg => cfg.AddProfile<MappingProfile>());
+            return config.CreateMapper();
+        }
+    }
+}
+
